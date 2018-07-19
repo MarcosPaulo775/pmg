e8d3f50651bb5da5e7c6862047c2d9e3c35aef3a
leofdss <leonardo.fariasdesouza2@gmail.com>
2018-07-19 15:56:10 -0300

    Create report

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material';
+import { ReportComponent } from './components/report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
