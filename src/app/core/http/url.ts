d0da88e8c9ff42d421ef3f3fae2102c8dee9971e
leofdss <leonardo.fariasdesouza2@gmail.com>
2018-08-19 11:05:12 -0300

    update angular 6.1.4

--- src/app/core/http/url.ts
+++ src/app/core/http/url.ts
@@ -3,3 +3,4 @@ export const server = '/api/portal.cgi/';
 //chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
 //corsproxy
 //ng serve --host 0.0.0.0 --disable-host-check --proxy-config proxy.config.json
+//git config --global credential.helper cache
