ba33567bb5eb8024532e0b2619b5469c49587efe
leofdss <leonardo.fariasdesouza2@gmail.com>
2018-10-31 10:22:57 -0300

    form

--- src/app/shared/models/os.ts
+++ src/app/shared/models/os.ts
@@ -11,6 +11,55 @@ export class Os {
   detail?: Detail;
   deleted?: string;
   status?: string;
+
+  //cliche
+  tecnologia?: string;
+  varicacao?: string;
+  material?: string;
+  lineatura?: string;
+  espessura?: string;
+  camada?: string;
+  local?: string;
+  obs_cliche?: string;
+
+  //cores
+  colors?: Color[];
+  obs_color?: string;
+  perfil?: string;
+
+  //montagem
+  fechamento?: boolean;
+  desenvolvimento?: string;
+  pistas?: string;
+  peso?: string;
+  gap?: string;
+  largura?: number;
+  altura?: number;
+  largura_material?: number;
+
+  esquerda?: boolean;
+  direita?: boolean;
+  topo?: boolean;
+  base?: boolean;
+
+  esquerda_mm?: number;
+  direita_mm?: number;
+  topo_mm?: number;
+  base_mm?: number;
+
+  refile?: boolean;
+  corte?: boolean;
+  cameron?: boolean;
+  microponto?: boolean;
+
+  //Prova
+
+  tinta?: string;
+  substrato?: string;
+  velocidade?: string;
+  dupla_face?: string;
+  temperatura?: string;
+  horario?: string;
   error_code?: string;
   error?: string;
 }
