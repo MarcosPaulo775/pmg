03baec1030aebc507c7777855af785ebc0e96e8e
leofdss <leonardo.fariasdesouza2@gmail.com>
2018-11-05 17:56:31 -0200

    form basicos

--- src/app/components/register/component/register.component.ts
+++ src/app/components/register/component/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  basicos: FormGroup;
+  cadastrais: FormGroup;
+  pagamento: FormGroup;
+  preco: FormGroup;
+  outros: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.basicos = this.formBuilder.group({
+      solicitante: [null, []],
+      novo: [null, []],
+      fisica: [null, []],
+      juridica: [null, []],
+      principal: [null, []],
+    });
+
+    this.cadastrais = this.formBuilder.group({
+      razao: [null, []],
+      fantasia: [null, []],
+      endereco: [null, []],
+      bairro: [null, []],
+      cidade: [null, []],
+      uf: [null, []],
+      cep: [null, []],
+      tel: [null, []],
+      fax: [null, []],
+      cnpj_cpf: [null, []],
+      ie_rg: [null, []],
+      comercial: [null, []],
+      tel_comercial: [null, []],
+      email_comercial: [null, []],
+      financeiro: [null, []],
+      tel_financeiro: [null, []],
+      email_financeiro: [null, []],
+      endereco_cobranca: [null, []],      
+    });
+
+    this.pagamento = this.formBuilder.group({
+      nf: [null, []],
+      boleto: [null, []],
+      prazo: [null, []],
+    });
+
+    this.preco = this.formBuilder.group({
+      digital_114: [null, []],
+      digital_170: [null, []],
+      digital_284: [null, []],
+      kodak_114: [null, []],
+      kodak_170: [null, []],
+      margem: [null, []],
+    });
+
+    this.outros = this.formBuilder.group({
+      email_nf: [null, []],
+      email_materiais: [null, []],
+      email_pedido: [null, []],
+      serasa: [null, []],
+      cartao_cnpj: [null, []],
+      obs_financeiro: [null, []],
+      obs_diretoria: [null, []],
+    });
   }
 
   users: string[] = [
---
+++ src/app/shared/models/company.ts
@@ -0,0 +1,47 @@
+export class Company {
+    _id?: string;
+    
+    solicitante?: string;
+    novo?: string;
+    fisica?: string;
+    juridica?: string;
+    principal?: string;
+    razao?: string;
+    fantasia?: string;
+    endereco?: string;
+    bairro?: string;
+    cidade?: string;
+    uf?: string;
+    cep?: string;
+    tel?: string;
+    fax?: string;
+    cnpj_cpf?: string;
+    ie_rg?: string;
+    comercial?: string;
+    tel_comercial?: string;
+    email_comercial?: string;
+    financeiro?: string;
+    tel_financeiro?: string;
+    email_financeiro?: string;
+    endereco_cobranca?: string;
+    nf?: string;
+    boleto?: string;
+    prazo?: string;
+    digital_114?: string;
+    digital_170?: string;
+    digital_284?: string;
+    kodak_114?: string;
+    kodak_170?: string;
+    margem?: string;
+    email_nf?: string;
+    email_materiais?: string;
+    email_pedido?: string;
+    serasa?: string;
+    cartao_cnpj?: string;
+    obs_financeiro?: string;
+    obs_diretoria?: string;
+
+    error_code?: string;
+    error?: string;
+}
+
