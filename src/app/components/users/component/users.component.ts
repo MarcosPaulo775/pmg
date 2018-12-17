784c711180c6f55df86b369f70b60d268aa7fbce
leofdss <leonardo.fariasdesouza2@gmail.com>
2018-12-17 09:00:06 -0200

    Listar usuarios

--- src/app/components/users/component/users.component.ts
+++ src/app/components/users/component/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigComponent } from '../../config/component/config.component';
+import { AuthService } from 'src/app/core/authentication/auth.service';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public configComponent: ConfigComponent,
+    public authService: AuthService
+  ) { }
+
+  users: User[];
 
   ngOnInit() {
+    this.configComponent.title = 'Usuários';
+    this.configComponent.user_color = '';
+    this.configComponent.users_color = 'rgb(0, 90, 176)';
+
+    this.authService.users_list_users()
+      .subscribe((data: User[]) => {
+        if (data[0]) {
+          this.users = data;
+        }
+      }, (data) => {
+      })
   }
 
 }
